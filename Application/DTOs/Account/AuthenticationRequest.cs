86de3ad1ad0e72689e3b0a212f6eeaf70e89f588
Kayky Barbosa <109919604+kaykybarbosa@users.noreply.github.com>
2023-06-21 09:03:13 -0300

    Update AuthenticationRequest.cs

--- Application/DTOs/Account/AuthenticationRequest.cs
+++ Application/DTOs/Account/AuthenticationRequest.cs
@@ -7,6 +7,6 @@ namespace Application.DTOs.Account
     public class AuthenticationRequest
     {
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string Password { get; set;}
     }
 }
